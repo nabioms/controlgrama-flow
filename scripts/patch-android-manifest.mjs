c7c434c66778321b910d93ce110451cd9a8bced3
nabioms <cr.rhcontato@gmail.com>
2026-09-22 00:50:19 -0300

    feat: set ControlGrama Android launcher icon

--- scripts/patch-android-manifest.mjs
+++ scripts/patch-android-manifest.mjs
@@ -1,7 +1,20 @@
-import { readFile, writeFile } from "node:fs/promises";
+import { mkdir, readFile, writeFile } from "node:fs/promises";
 
 const manifestPath = "android/app/src/main/AndroidManifest.xml";
+const iconPath = "android/app/src/main/res/drawable/ic_controlgrama.xml";
+const iconVector = "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n<vector xmlns:android=\"http://schemas.android.com/apk/res/android\"\n    android:width=\"108dp\"\n    android:height=\"108dp\"\n    android:viewportWidth=\"512\"\n    android:viewportHeight=\"512\">\n    <path android:fillColor=\"#05090A\" android:pathData=\"M112,0 L400,0 Q512,0 512,112 L512,400 Q512,512 400,512 L112,512 Q0,512 0,400 L0,112 Q0,0 112,0Z\"/>\n    <path android:fillColor=\"#69E52D\" android:fillType=\"evenOdd\" android:pathData=\"M256,76 A160,160 0,1 1,96,236 A160,160 0,0 1,256,76 M256,124 A112,112 0,1 0,368,236 A112,112 0,0 0,256,124Z\"/>\n    <path android:fillColor=\"#05090A\" android:pathData=\"M300,52 L512,52 L512,215 L335,215 L300,180Z\"/>\n    <path android:fillColor=\"#69E52D\" android:pathData=\"M150,248 L235,333 L405,163 L405,220 L235,390 L150,305Z\"/>\n    <path android:fillColor=\"#F7FAF8\" android:pathData=\"M205,238 L205,177 L237,148 L237,275Z\"/>\n    <path android:fillColor=\"#F7FAF8\" android:pathData=\"M256,279 L256,130 L288,99 L288,312Z\"/>\n    <path android:fillColor=\"#F7FAF8\" android:pathData=\"M307,319 L307,83 L339,52 L339,352Z\"/>\n    <path android:fillColor=\"#69E52D\" android:pathData=\"M165,313 C214,333 262,350 314,350 C349,350 383,340 419,321 C386,372 338,394 282,394 C229,394 184,370 145,340Z\"/>\n</vector>";
+
 let manifest = await readFile(manifestPath, "utf8");
+
+await mkdir("android/app/src/main/res/drawable", { recursive: true });
+await writeFile(iconPath, iconVector);
+
+if (!manifest.includes('android:icon="@drawable/ic_controlgrama"')) {
+  manifest = manifest.replace(
+    /(<application[^>]*?)android:label=/,
+    '$1android:icon="@drawable/ic_controlgrama" android:roundIcon="@drawable/ic_controlgrama" android:label='
+  );
+}
 
 const marker = "<!-- ControlGrama deep link -->";
 if (!manifest.includes(marker)) {
@@ -18,5 +31,6 @@ if (!manifest.includes(marker)) {
   const activityEnd = manifest.indexOf("</activity>");
   if (activityEnd === -1) throw new Error("Main activity not found in AndroidManifest.xml");
   manifest = manifest.slice(0, activityEnd) + intentFilter + manifest.slice(activityEnd);
-  await writeFile(manifestPath, manifest);
 }
+
+await writeFile(manifestPath, manifest);
